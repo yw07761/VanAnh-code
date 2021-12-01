1f1a232bf50c12fba1b3e8ebb9f926c22f2f4e4d
yw07761 <89188722+yw07761@users.noreply.github.com>
2021-12-02 00:43:28 +0700

    Add files via upload

--- OPP/hoHoangVanAnh/bai07/Employee.java
+++ OPP/hoHoangVanAnh/bai07/Employee.java
@@ -1,64 +1,113 @@
 package hoHoangVanAnh.bai07;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Objects;
+
 public abstract class Employee {
-	private String firstName;
-    private String lastName;
-    private String socialSecurityNumber;
-    
-	public Employee(String firstName, String lastName, String socialSecurityNumber) {
-		super();
-		setFirstName(firstName);
-		setLastName(lastName);
-		setSocialSecurityNumber(socialSecurityNumber);
+	 protected int thePayrollNumber; //Mã số
+	 protected String theName; //Tên Nhân Viên
+	 protected double theBasicMounthlySalary; // Lương Cơ Bản
+	 
+	 /**
+	  * hàm khởi tạo 
+	  * @param thePayrollNumber
+	  * @param theName
+	  * @param theBasicMounthlySalary
+	  */
+	public Employee(int thePayrollNumber, String theName, double theBasicMounthlySalary) {
+		setThePayrollNumber(thePayrollNumber);
+		setTheName(theName);
+		setTheBasicMounthlySalary(theBasicMounthlySalary);
 	}
 
 	public Employee() {
-		this("un-known", "un-known", "un-known");
+		this(0,"xxx",0);
 	}
 
-	public String getFirstName() {
-		return firstName;
+	public int getThePayrollNumber() {
+		return thePayrollNumber;
 	}
-
-	public void setFirstName(String firstName) {
-		if(!firstName.equals(""))
-			this.firstName = firstName;
+	/**
+	 * ktr Mã số > 0
+	 * @param thePayrollNumber
+	 */
+	public void setThePayrollNumber(int thePayrollNumber) {
+		if(thePayrollNumber > 0)
+			this.thePayrollNumber = thePayrollNumber;
 		else
-			this.firstName = "un-known";
+			this.thePayrollNumber = 0;
 	}
 
-	public String getLastName() {
-		return lastName;
+	public String getTheName() {
+		return theName;
 	}
-
-	public void setLastName(String lastName) {
-		if(!lastName.equals(""))
-			this.lastName = lastName;
+	/**
+	 * ktr tên có rỗng hay không
+	 * @param theName
+	 */
+	public void setTheName(String theName) {
+		if(!theName.equals(""))
+			this.theName = theName;
 		else
-			this.lastName = "un- knowm";
+			this.theName = "xxx";
 	}
 
-	public String getSocialSecurityNumber() {
-		return socialSecurityNumber;
+	public double getTheBasicMounthlySalary() {
+		return theBasicMounthlySalary;
 	}
-
-	public void setSocialSecurityNumber(String socialSecurityNumber) {
-		if(!socialSecurityNumber.equals(""))
-			this.socialSecurityNumber = socialSecurityNumber;
+	/**
+	 * ktr lương cơ bản  > 0
+	 * @param theBasicMounthlySalary
+	 */
+	public void setTheBasicMounthlySalary(double theBasicMounthlySalary) {
+		if(theBasicMounthlySalary > 0)
+			this.theBasicMounthlySalary = theBasicMounthlySalary;
 		else
-			this.socialSecurityNumber = "un-known";
+			this.theBasicMounthlySalary = 0;
+	}
+	
+	public abstract double getMonthlySalary();
+	
+	@Override
+	public String toString() {
+		Locale locale = new Locale("en", "US");      
+        NumberFormat currency = NumberFormat.getCurrencyInstance(locale);
+        return String.format("%-20d %-30s %10s",thePayrollNumber,theName,
+        		currency.format(theBasicMounthlySalary));
+	}
+	public static String getTieuDe() {
+        return String.format("%-20s %-18s %20s\n","Mã NV","Tên NV","Lương CB");    
+    }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(theBasicMounthlySalary);
 	}
 
 	@Override
-	public String toString() {
-		return String.format("%s %s\nsocial security number: %s", 
-				firstName,lastName,socialSecurityNumber);
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Double.doubleToLongBits(theBasicMounthlySalary) == Double.doubleToLongBits(other.theBasicMounthlySalary);
+	}
+
+	public double getMounthSalary() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
-	public abstract double earnings();
 	
-    
 	
-    
+	
+	
+	
+	 
+	
 }
 
--- OPP/hoHoangVanAnh/bai07/Test.java
+++ OPP/hoHoangVanAnh/bai07/Test.java
@@ -1,53 +1,95 @@
 package hoHoangVanAnh.bai07;
 
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Salaried salariedEmployee = new Salaried( "John", "Smith", "111-11-1111", 2000); 
-		Hourly hourlyEmployee = new Hourly( "Karen", "Price", "222-22-2222", 20, 40 );
-		Commission commissionEmployee = 
-				new Commission( "Sue", "Jones", "333-33-3333", 10000, .06 ); 
-				BasePlusCommission basePlusCommissionEmployee = 
-				new BasePlusCommission( "Bob", "Lewis", "444-44-4444", 5000, .04, 300 ); 
-				
-		System.out.println( "Employees processed individually:\n" );
-			
-		System.out.printf( "%s\n%s: $%,.2f\n\n",
-		salariedEmployee, "earned", salariedEmployee.earnings() );
-		System.out.printf( "%s\n%s: $%,.2f\n\n",
-		hourlyEmployee, "earned", hourlyEmployee.earnings() );
-		System.out.printf( "%s\n%s: $%,.2f\n\n",
-		commissionEmployee, "earned", commissionEmployee.earnings() );
-		System.out.printf( "%s\n%s: $%,.2f\n\n",
-		basePlusCommissionEmployee,
-				"earned", basePlusCommissionEmployee.earnings() );
-			
-		Employee employees[] = new Employee[ 4 ];
-		 
-		employees[ 0 ] = salariedEmployee; 
-		employees[ 1 ] = hourlyEmployee; 
-		employees[ 2 ] = commissionEmployee; 
-		employees[ 3 ] = basePlusCommissionEmployee;
-				
-		System.out.println( "Employees processed polymorphically:\n" );
-				
-		for ( Employee currentEmployee : employees )
-		{
-			System.out.println( currentEmployee );
+	SoftwareHouse a = new SoftwareHouse("FPT");
+	a.ThemEmp(new Programmer(1, "Nguyễn Văn Nam",30000, "Java"));
+	a.ThemEmp(new Administrator(2, "Trần Chí Khang", 40000, "Lầu 2"));
+	a.ThemEmp(new ProjectLeader(3, "Nguyễn Văn Toàn", 50000, "Python"));
 	
-			if ( currentEmployee instanceof BasePlusCommission )
-			{
-				BasePlusCommission employee = ( BasePlusCommission ) currentEmployee;
-	
-				double oldBaseSalary = employee.getBaseSalary();
-				employee.setBaseSalary(1.1 * oldBaseSalary);
-				System.out.println("new base salary with 10% increase is: " + employee.getBaseSalary());
+	Scanner sc = new Scanner(System.in);
+	int n;
+	do {
+		System.out.println("\n_____MENU EMPLOYEE_____");
+		System.out.println("1. Thêm 1 Nhân Viên vào công ty");
+		System.out.println("2. Xuất toàn bộ thông tin nhân viên");
+		System.out.println("3. Tính tổng tiền phải trả cho nhân viên");
+		System.out.println("4. Sắp xếp danh sách nhân viên theo tên");
+		System.out.println("5. Trả về thông tin các lập trình viên");
+		System.out.println("6. Cập nhật phòng ban");
+		System.out.println("7. Xóa nhân viên theo mã số");
+		System.out.println("0. Thoát");
+		System.out.print("\n\tMời nhập lựa chọn: ");
+		n = sc.nextInt();
+		switch (n) {
+		case 1:
+			 System.out.print("\nNhập mã số nhân viên: ");
+             int id = sc.nextInt();
+             System.out.print("\nNhập tên nhân viên: ");
+             sc.nextLine();
+             String tenNV = sc.nextLine();
+             System.out.print("\nNhập lương cơ bản: ");
+             double luongCB = sc.nextDouble();
+             System.out.print("Nhập ngôn ngữ: ");
+             sc.nextLine();
+             String ngonNgu = sc.nextLine();
+             if (a.ThemEmp(new Programmer(id, tenNV, luongCB, ngonNgu))) {
+                 System.out.println("Danh sách sau khi tiêm: ");
+                 System.out.println(a);
+             } else 
+                 System.out.println("Thêm không thành công");
+             break;
+		case 2:
+			System.out.println(a.displayAllStaff());
+			break;
+		case 3:
+			Locale locale = new Locale("en", "US");      
+            NumberFormat currency = NumberFormat.getCurrencyInstance(locale);
+            System.out.printf("Tổng tiền phải trả cho nhân viên: %s \n",
+            		currency.format(a.getMonthlySalaryBill()));
+            break;
+		case 4:
+			a.xapdanhsachtheoten();
+			System.out.println(a);
+		case 5:
+			System.out.println(a.getThongTinLapTrinhVien());
+		case 6:
+			System.out.print("\nnhập mã số cần tìm");
+			int ID = sc.nextInt();
+			System.out.print("Nhập Phòng ban mới");
+			sc.nextLine();
+			String deptNew = sc.nextLine();
+			if(a.updateDepartmentForAdmin(ID, deptNew)) {
+				System.out.println("	sữa thành công");
+				System.out.println(a);
+			}else {
+				System.out.println("	sữa không thành công");
 			}
-			System.out.printf("earned $%,.2f\n\n", currentEmployee.earnings() );
+			break;
+		case 7:
+			System.out.print("\nNhập mã cần xóa: ");
+			int iD = sc.nextInt();
+			if(a.deleteEmployee(iD)) {
+				System.out.println("	xóa thành công");
+				System.out.println(a);
+			}else {
+				System.out.println("	xóa không thành công");
+			}
+		case 0:
+			sc.close();
+			System.out.println("\nKết thúc");
+			return;
 		}
-		
-	}
+	} while(n != 0);
+	System.out.println("nhập lại"); 
 	}
 
+}
 
